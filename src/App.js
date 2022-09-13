176525f45893d17f3c646fa55318899e45ab83a5
Aiymkan <altish536@gmail.com>
2022-09-13 15:57:57 +0600

    HW1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+function AboutUs() {
+  return (
+    <div className="name">
+      <h1>Save Your Tears</h1>
+      <h2>Song by The Weeknd</h2>
+      <p>Take me back 'cause I wanna stay</p>
+      <p> Save your tears for another day</p>
+    </div>
+  );
+}
+
+function Portfolio() {
+  return (
+    <div className="first">
+      <h6>Source: LyricFind</h6>
+      <h6>
+        Songwriters: Abel Tesfaye / Ahmad Balshe / Jason Matthew Quenneville /
+        Martin Karl Sandberg / Oscar Thomas Holter
+      </h6>
+      <h6>
+        Save Your Tears lyrics © Universal Music Publishing Group, Warner
+        Chappell Music, Inc
+      </h6>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AboutUs />
+      <Portfolio />
     </div>
   );
 }
